c1e0323eec13c88775b27f193e336f5e3ddf755a
zhuravlev12 <slawajur@mail.ru>
2020-05-01 17:52:29 +0700

    added resource view

---
+++ RollCreators/Assets/RollCreators/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public enum DayTime
+    {
+        DAY,
+        NIGHT
+    }
+    
+    public int sinners;
+    public int gold;
+    public int attention;
+    public int agentsDay;
+    public int agentsNight;
+    public Text sinnersText;
+    public Text goldText;
+    public Text attentionText;
+    public Text agentsText;
+    public GameObject dayDoingsPanel;
+    public GameObject nightDoingsPanel;
+    private DayTime dayTime = DayTime.DAY;
+
+    void Update()
+    {
+        sinnersText.text = $"Sinners: {sinners}";
+        goldText.text = $"Gold: {gold}";
+        attentionText.text = $"Attention: {attention}";
+        agentsText.text = $"Agents: {(dayTime == DayTime.DAY ? agentsDay : agentsNight)}";
+    }
+
+    public void ChangeDayTime()
+    {
+        if (dayTime == DayTime.DAY)
+        {
+            dayDoingsPanel.SetActive(false);
+            nightDoingsPanel.SetActive(true);
+            dayTime = DayTime.NIGHT;
+        }
+        else
+        {
+            dayDoingsPanel.SetActive(true);
+            nightDoingsPanel.SetActive(false);
+            dayTime = DayTime.DAY;
+        }
+    }
+    
+}
