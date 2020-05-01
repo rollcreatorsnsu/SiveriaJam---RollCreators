6bbbdcda0f8a0a6dec2c7d0d4f8cc7f485079932
zhuravlev12 <slawajur@mail.ru>
2020-05-01 16:38:35 +0700

    added main menu

---
+++ RollCreators/Assets/RollCreators/Scripts/UI/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string gameSceneName = "Game";
+    
+    public void _Start()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
