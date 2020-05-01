2a42a82789ff161cf6bee8461bedfb32590ee46b
zhuravlev12 <slawajur@mail.ru>
2020-05-01 17:09:18 +0700

    added settings button

---
+++ RollCreators/Assets/RollCreators/Scripts/UI/GameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class GameMenu : MonoBehaviour
+{
+    public string menuSceneName = "Menu";
+    public GameObject settingsPanel;
+
+    public void ShowSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+    
+    public void Mute(bool isOn)
+    {
+        AudioListener.volume = isOn ? 1.0f : 0.0f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
