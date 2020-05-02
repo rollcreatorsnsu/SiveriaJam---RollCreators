50c4ca92529c12b04c91d27c9895b997f03640f0
zhuravlev12 <slawajur@mail.ru>
2020-05-02 20:30:59 +0700

    actualized night doings

--- RollCreators/Assets/RollCreators/Scripts/Entities/NightAgent.cs
+++ RollCreators/Assets/RollCreators/Scripts/Entities/NightAgent.cs
@@ -27,45 +27,45 @@ public class NightAgent : Agent
         {
             case (NightTask.OPEN_FLAT):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.VANITY] += Random.Range(-30, 10) + 5 * skills[Skills.ELOQUENCE];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.VANITY] += Random.Range(-30, 10) + 2 * (skills[Skills.ELOQUENCE] + game.sinners[tempSocialStatus].sins[Sinner.Sins.VANITY] / 10);
                 game.attention += Random.Range(5, 15);
                 break;
             }
             case (NightTask.MUCHLY_PRAISE):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.ENVY] += Random.Range(-30, 10) + 5 * skills[Skills.WISDOM];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.ENVY] += Random.Range(-30, 10) + 2 * (skills[Skills.WISDOM] + game.sinners[tempSocialStatus].sins[Sinner.Sins.ENVY] / 10);
                 game.attention += Random.Range(5, 15);
                 break;
             }
             case (NightTask.PROVOKE_TO_FIGHT):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.ANGER] += Random.Range(-30, 10) + 5 * skills[Skills.PRESSURE];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.ANGER] += Random.Range(-30, 10) + 2 * (skills[Skills.PRESSURE] + game.sinners[tempSocialStatus].sins[Sinner.Sins.ANGER] / 10);
                 game.attention += Random.Range(10, 20);
                 break;
             }
             case (NightTask.COMPLAINT_ON_JUSTICE):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.GLOOM] += Random.Range(-30, 10) + 5 * skills[Skills.PERSUASIVENESS];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.GLOOM] += Random.Range(-30, 10) + 2 * (skills[Skills.PERSUASIVENESS] + game.sinners[tempSocialStatus].sins[Sinner.Sins.GLOOM] / 10);
                 game.attention += Random.Range(5, 15);
                 break;
             }
             case (NightTask.DICE):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.GREED] += Random.Range(-30, 10) + 5 * skills[Skills.CUNNING];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.GREED] += Random.Range(-30, 10) + 2 * (skills[Skills.CUNNING] + game.sinners[tempSocialStatus].sins[Sinner.Sins.GREED] / 10);
                 game.attention += Random.Range(0, 15);
                 game.gold -= 25;
                 break;
             }
             case (NightTask.TAKE_A_BREAK):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.GLUTTONY] += Random.Range(-30, 10) + 5 * skills[Skills.INSIGHT];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.GLUTTONY] += Random.Range(-30, 10) + 2 * (skills[Skills.INSIGHT] + game.sinners[tempSocialStatus].sins[Sinner.Sins.GLUTTONY] / 10);
                 game.attention += Random.Range(0, 10);
                 game.gold -= 50;
                 break;
             }
             case (NightTask.DEVELOP):
             {
-                game.sinners[tempSocialStatus].sins[Sinner.Sins.FORNICATION] += Random.Range(-30, 10) + 5 * skills[Skills.CHARM];
+                game.sinners[tempSocialStatus].sins[Sinner.Sins.FORNICATION] += Random.Range(-30, 10) + 2 * (skills[Skills.CHARM] + game.sinners[tempSocialStatus].sins[Sinner.Sins.FORNICATION] / 10);
                 game.attention += Random.Range(0, 10);
                 game.gold -= 50;
                 break;
