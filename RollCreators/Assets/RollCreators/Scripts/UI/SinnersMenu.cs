f76e3c9620faedea6ff75f158d9ad8a5d32d1220
zhuravlev12 <slawajur@mail.ru>
2020-05-03 19:46:21 +0700

    percents instead of /100

--- RollCreators/Assets/RollCreators/Scripts/UI/SinnersMenu.cs
+++ RollCreators/Assets/RollCreators/Scripts/UI/SinnersMenu.cs
@@ -58,13 +58,13 @@ public class SinnersMenu : MonoBehaviour
         Sinner sinner = game.sinners[STATUSES[index]];
         wealth.text = $"Богатство: {(sinner.wealthOpened ? sinner.wealth.ToString() : "???")}";
         fearOfGod.text = $"Богобоязненность: {(sinner.fearOfGodOpened ? sinner.fearOfGod.ToString() : "???")}";
-        vanity.text = $"Гордыня: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.VANITY].ToString() : "???")}/100";
-        envy.text = $"Зависть: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.ENVY].ToString() : "???")}/100";
-        anger.text = $"Гнев: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.ANGER].ToString() : "???")}/100";
-        gloom.text = $"Уныние: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GLOOM].ToString() : "???")}/100";
-        greed.text = $"Алчность: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GREED].ToString() : "???")}/100";
-        gluttony.text = $"Чревоугодие: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GLUTTONY].ToString() : "???")}/100";
-        fornication.text = $"Блуд: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.FORNICATION].ToString() : "???")}/100";
+        vanity.text = $"Гордыня: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.VANITY].ToString() : "???")}%";
+        envy.text = $"Зависть: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.ENVY].ToString() : "???")}%";
+        anger.text = $"Гнев: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.ANGER].ToString() : "???")}%";
+        gloom.text = $"Уныние: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GLOOM].ToString() : "???")}%";
+        greed.text = $"Алчность: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GREED].ToString() : "???")}%";
+        gluttony.text = $"Чревоугодие: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.GLUTTONY].ToString() : "???")}%";
+        fornication.text = $"Блуд: {(sinner.sinsOpened ? sinner.sins[Sinner.Sins.FORNICATION].ToString() : "???")}%";
         description.text = DESCRIPTIONS[index];
     }
 
